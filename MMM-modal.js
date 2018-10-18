36dffcbd0ce2aa4ec439a6c3da5e977e5db2f19e
AWEN <awen190418@gmail.com>
2018-10-18 13:54:38 +0900

    Added Audio Player

--- MMM-modal.js
+++ MMM-modal.js
@@ -15,7 +15,7 @@ Module.register("MMM-modal", {
 		return ["jquery-3.3.1.min.js"]
 	},*/
 	getStyles:function(){
-		return ["modal.css"]
+		return ["modal.css","style.css"]
 	},
 	getDom: function () {
 		var wrapper = document.createElement("div");
@@ -28,15 +28,62 @@ Module.register("MMM-modal", {
 				"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4",
 				"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4"
 			];
+		var AudioLinks=[
+				"http://localhost:8080/1.mp3",
+				"http://localhost:8080/2.mp3",
+				"http://localhost:8080/3.mp3",
+				"http://localhost:8080/4.mp3",
+				"http://localhost:8080/5.mp3",
+				"http://localhost:8080/6.mp3",
+				"http://localhost:8080/7.mp3",
+				"http://localhost:8080/8.mp3",
+				"http://localhost:8080/9.mp3",
+			];
+
 
 		//codechange
-	        var title = document.createElement("div");
-                title.className = "bright medium regular fed";
-                title.setAttribute("id","player");
-		title.innerHTML = "Today's Video";
-                title.addEventListener("click", () => showdesc(this)); //Show description on click
-                wrapper.appendChild(title);
- 
+	        var media = document.createElement("div");
+	        var videoC = document.createElement("div");
+		videoC.innerHTML = "Today's Video";
+		
+		videoC.className = "thin medium  bright pre-line fed";
+			
+                videoC.addEventListener("click", () => showdesc(this)); //Show description on click
+                media.appendChild(videoC);
+		wrapper.appendChild(media);
+ 		
+		//Control buttons.
+		var controls = document.createElement("div");
+		controls.className="controls fed";
+		var play = document.createElement("span");
+		var musicName = document.createElement("div");
+		var next = document.createElement("span");
+		var prev = document.createElement("span");
+		var stop= document.createElement("span");
+		
+				
+		play.className="icon-play3";
+		//pause.className="icon-pause2";
+		next.className="icon-next2";
+		prev.className="icon-previous2";
+		stop.className="icon-stop2";
+		
+
+		play.addEventListener("click", () => playM(this));  //Hide description on click
+		//pause.addEventListener("click", () => pauseM(this));  //Hide description on click
+		next.addEventListener("click", () => nextM(this));  //Hide description on click
+		prev.addEventListener("click", () => prevM(this));  //Hide description on click
+		stop.addEventListener("click", () => stopM(this));  //Hide description on click
+	
+		controls.appendChild(prev);
+		controls.appendChild(play);
+		//controls.appendChild(pause);
+		controls.appendChild(next);
+		//controls.appendChild(stop);
+		media.appendChild(musicName);
+		media.appendChild(controls);
+		
+			
               	
 		description = document.createElement("div");
                 description.className = "modalBox";
@@ -55,6 +102,16 @@ Module.register("MMM-modal", {
 		video.appendChild(source);
 		description.appendChild(video);
 
+
+
+		var audio = document.createElement("audio");
+		var AudioSource = document.createElement('source');
+		AudioSource.setAttribute("src",AudioLinks[Math.floor(Math.random()*AudioLinks.length)]);
+		AudioSource.setAttribute("type","audio/mpeg");
+		audio.appendChild(AudioSource);
+		
+
+
                 //below is the function to show description and hide title
                 function showdesc(thisdesc) {
                       	
@@ -62,7 +119,7 @@ Module.register("MMM-modal", {
 			video.autoplay=true;
 			video.load();                       
  			description.style.display="";
-			title.style.display="none";
+			videoC.style.display="none";
             		console.log("SHOW"); 
 		};
  
@@ -70,16 +127,58 @@ Module.register("MMM-modal", {
                  function hidedesc(thisdesc) {
                   
 			video.pause();                      	
-			title.style.display="";
+			videoC.style.display="";
 			description.style.display="none";
                        
             		console.log("HIDE"); 
 		};
 
 		//codechange end
+		
 
-
-
+		//Music Controls
+		function playM(thisobj){
+			musicName.innerHTML = /[^/]*$/.exec(AudioSource.src)[0];
+			if(play.classList.contains('icon-play3')){
+				audio.autoplay=true;
+				audio.load();
+				play.classList.remove('icon-play3');
+				play.classList.add('icon-pause2');
+			}
+			else{
+			
+				play.classList.remove('icon-pause2');
+				play.classList.add('icon-play3');
+				audio.pause();
+					
+			}
+			console.log("PLAY");
+		};
+		function pauseM(thisobj){
+			musicName.innerHTML = "";
+			audio.pause();
+			console.log("PAUSE");
+		};
+		function stopM(thisobj){
+			musicName.innerHTML = "";
+			audio.pause();
+			audio.currenttime=0;
+			console.log("STOP");
+		};
+		function prevM(thisobj){
+			
+			musicName.innerHTML = /[^/]*$/.exec(AudioSource.src)[0];
+			AudioSource.setAttribute("src",AudioLinks[Math.floor(Math.random()*AudioLinks.length)]);
+			audio.load();
+			console.log("PREV");
+		};
+		function nextM(thisobj){
+			musicName.innerHTML = /[^/]*$/.exec(AudioSource.src)[0];
+			console.log("Next");
+			AudioSource.setAttribute("src",AudioLinks[Math.floor(Math.random()*AudioLinks.length)]);
+			audio.load();
+		};
+		//Music Controls End
 
 
 
